53e11c89a7efbd68b927d36ee36e73f909002e21
Lotti Winter <whimsy.lieselotte@gmail.com>
2022-09-30 18:16:44 +0400

    User register system

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("user_login/", views.user_login, name="login"),
     path("user_logout/", views.user_logout, name="logout"),
+    path("user_register/", views.user_register, name="register"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.http import HttpResponse, HttpRequest
 
@@ -22,4 +23,27 @@ def user_login(request: HttpRequest) -> HttpResponse:
 def user_logout(request: HttpRequest) -> HttpResponse:
     logout(request)
     messages.success(request, "Logged out")
-    return redirect('home')
+    return redirect("home")
+
+
+def user_register(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():  # TODO: indent remove
+            form.save()
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+            print(form)
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Registered"))
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+    return render(
+        request,
+        "registration/register.html",
+        {
+            "form": form,
+        },
+    )
