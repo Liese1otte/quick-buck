b6df868aaa990387b40dde69b58f650ee0e1016c
Lotti Winter <whimsy.lieselotte@gmail.com>
2022-10-02 17:10:36 +0400

    Change to weekday date system

--- posts/models.py
+++ posts/models.py
@@ -3,12 +3,24 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
+DAYS_OF_WEEK = (
+    (0, 'Monday'),
+    (1, 'Tuesday'),
+    (2, 'Wednesday'),
+    (3, 'Thursday'),
+    (4, 'Friday'),
+    (5, 'Saturday'),
+    (6, 'Sunday'),
+)
+
+
 class PostModel(models.Model):
     teacher = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Teacher")
     student = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Student")
     lesson_type = models.CharField(max_length=255)
     lesson_name = models.CharField(max_length=255)
-    lesson_time = models.DateTimeField()
+    lesson_week = models.CharField(max_length=1, choices=DAYS_OF_WEEK)
+    lesson_time = models.TimeField()
     payment_status = models.BooleanField(default=False)
     payment_due_to = models.DateField()
 
